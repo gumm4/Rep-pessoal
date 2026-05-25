ef1f0cd653e63292f6cf8adc9d79a65aeab478c3
gumm4 <luizduaetee@gmail.com>
2026-05-25 21:59:01 +0000

    Melhoras em alguns aspectos visuais, e implementação de um chat que conta sobre alguns projetos e tecnologias

--- Portfolio-curso/js/index.js
+++ Portfolio-curso/js/index.js
@@ -8,3 +8,40 @@ console.log("Body encontrado:", body);
 botao.addEventListener("click", () => {
     body.classList.toggle("dark_mode");
 });
+
+async function carregarDados() {
+
+    const resposta = await fetch("/Portfolio-curso/json/dados.json");
+    return await resposta.json();
+
+}
+
+async function enviar() {
+
+    const input = document.getElementById("input");
+    const texto = input.value.toLowerCase();
+
+    const dados = await carregarDados();
+
+    let resposta = "Não entendi";
+
+    if (texto.includes("tecnologia")) {
+        resposta = dados.tecnologias.join(", ");
+    }
+
+    if (texto.includes("quadras")) {
+        resposta = dados.projetos.quadras;
+    }
+
+    if (texto.includes("parking")) {
+        resposta = dados.projetos.estacionamento;
+    }
+
+    document.getElementById("mensagens").innerHTML += `
+
+        <p><strong>Você:</strong> ${texto}</p>
+        <p><strong>Bot:</strong> ${resposta}</p>
+    
+    `;
+
+}
