54712c9fa9b5f7441ade1e0e8de050e2a5df28d0
Luiz <luizudaetee@gmail.com>
2026-05-31 10:40:24 -0300

    Alteração na paleta de cores

---
+++ Rep-pessoal-main/docs/index.js
@@ -0,0 +1,51 @@
+const botao = document.getElementById("cortrocar");
+const body = document.getElementById("body");
+
+console.log("Botão encontrado:", botao);
+console.log("Body encontrado:", body);
+
+
+botao.addEventListener("click", () => {
+    body.classList.toggle("dark_mode");
+});
+
+async function carregarDados() {
+
+    const resposta = await fetch("./dados.json");
+    return await resposta.json();
+
+}
+
+async function enviar() {
+
+    const input = document.getElementById("input");
+    const texto = input.value.toLowerCase();
+
+    const dados = await carregarDados();
+
+    let resposta = "Não entendi";
+
+    if (texto.includes("tecnologia")) {
+        resposta = dados.tecnologias.join(", ");
+    }
+
+    if (texto.includes("quadras")) {
+        resposta = dados.projetos.quadras;
+    }
+
+    if (texto.includes("parking")) {
+        resposta = dados.projetos.estacionamento;
+    }
+
+    if (texto.includes("motivação")) {
+        resposta = dados.comecar.motivo;
+    }
+
+    document.getElementById("mensagens").innerHTML += `
+
+        <p class="usuario"><strong>Você:</strong> ${texto}</p>
+        <p class="bot"><strong>Bot:</strong> ${resposta}</p>
+    
+    `;
+
+}
